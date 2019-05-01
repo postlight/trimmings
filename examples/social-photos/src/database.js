3aed8a4e2673eafdab901040157d673e3698061d
John Holdun <john@johnholdun.com>
2019-05-01 16:51:02 -0400

    Use CSV for seed data

--- examples/social-photos/src/database.js
+++ examples/social-photos/src/database.js
@@ -1,43 +1,26 @@
-const DATA = {
-  photos: {
-    '1': {
-      url: '/photos/1.jpg',
-      caption: 'At the beach!',
-      userId: 'beach.lover',
-      credit: 'https://unsplash.com/photos/83zRhEhFMfo',
-      photographer: 'Sai Kiran Anagani',
-      likes: 1039,
-      commentsCount: 1
-    },
-    '2': {
-      url: '/photos/2.jpg',
-      caption: 'I freakin’ love the beach. Look at these surfboards.',
-      userId: 'beach.lover',
-      credit: 'https://unsplash.com/photos/VAggpKuA2zU',
-      photographer: 'Abigail Lynn',
-      likes: 1158,
-      commentsCount: 0
-    }
-  },
-  users: {
-    'beach.lover': {
-      name: 'Beach Lover 1996'
-    }
-  },
-  comments: {
-    '1': {
-      photoId: '1',
-      userId: 'beach.lover',
-      text: 'LOL I’m commenting on my own photo'
-    }
-  }
-}
+const csvParse = require('csv-parse/lib/sync')
+const fs = require('fs')
 
-Object.keys(DATA).forEach((type) => {
-  Object.keys(DATA[type]).forEach((id) => {
-    DATA[type][id] = { type, id, ...DATA[type][id] }
-  })
-})
+const DATA_DIRECTORY = './data'
+
+const DATA =
+  fs.readdirSync(DATA_DIRECTORY).reduce((acc, filename) => {
+    const records =
+      csvParse(
+        fs
+          .readFileSync(`${DATA_DIRECTORY}/${filename}`)
+          .toString(),
+        { columns: true }
+      )
+
+    const type = filename.replace(/\.csv/, '')
+
+    acc[type] = records.reduce((rAcc, record) => {
+      rAcc[record.id] = Object.assign({ type }, record)
+      return rAcc
+    }, {})
+    return acc
+  }, {})
 
 const database = {
   get: (type, id) => {
