bc58aa78770676ff0737b507a37bcc2a6e288510
John Holdun <john@johnholdun.com>
2019-05-06 17:49:24 -0400

    fix: Support fetch-based submission of GET forms

--- src/loadElement.js
+++ src/loadElement.js
@@ -1,18 +1,21 @@
 import load from './load'
 import serialize from './serialize'
 
+const POST_HEADERS = { 'Content-Type': 'application/x-www-form-urlencoded' }
+
 const loadElement = (element) => {
   if (element.nodeName === 'A') {
     return load(element.getAttribute('href'))
   } else if (element.nodeName === 'FORM') {
-    return load(
-      element.getAttribute('action'),
-      {
-        method: element.getAttribute('method'),
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: serialize(element)
-      }
-    )
+    const url = element.getAttribute('action')
+    const method = element.getAttribute('method')
+    const body = serialize(element)
+
+    if (!method || method.toLowerCase() === 'get') {
+      return load(`${url}?${body}`)
+    }
+
+    return load(url, { method, body, headers: POST_HEADERS })
   } else {
     throw new Error(`Unexpected nodename ${element.nodeName} for loadElement`)
   }
