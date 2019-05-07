3b10dd6bee53d9db06be57a54ff9dbeaaf6a48c0
John Holdun <john@johnholdun.com>
2019-05-07 13:46:21 -0400

    Test replace

---
+++ tests/replace.test.js
@@ -0,0 +1,10 @@
+test('replace', async () => {
+  const page = await browser.newPage()
+  await page.goto('http://localhost:4444/replace-1.html')
+  await expect(page).toClick('a')
+  await expect(page).not.toMatch('This is page 1.')
+  await expect(page).not.toMatch('This will always be page 1!')
+  await expect(page).toMatch('This is page 2.')
+  await expect(page).toMatch('This has always been page 2!')
+  await expect(page).toMatch('Thank you for your time.')
+})
