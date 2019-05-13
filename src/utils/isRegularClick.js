1a0e16d5160370aa4c8926e344b255bc4dca4e29
John Holdun <john@johnholdun.com>
2019-05-13 16:03:18 -0400

    Lint, whoops

--- src/utils/isRegularClick.js
+++ src/utils/isRegularClick.js
@@ -6,13 +6,13 @@
 // https://github.com/turbolinks/turbolinks/blob/26f42b148b624115b97e4d083f3a1f47d72a242f/src/controller.ts#L287
 const isRegularClick = (event) => {
   return !(
-    (event.target && event.target.isContentEditable)
-    || event.defaultPrevented
-    || event.which > 1
-    || event.altKey
-    || event.ctrlKey
-    || event.metaKey
-    || event.shiftKey
+    (event.target && event.target.isContentEditable) ||
+    event.defaultPrevented ||
+    event.which > 1 ||
+    event.altKey ||
+    event.ctrlKey ||
+    event.metaKey ||
+    event.shiftKey
   )
 }
 
