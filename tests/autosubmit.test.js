c325824b862b7dafbabb9c044700bd751138c8a9
John Holdun <john@johnholdun.com>
2019-12-05 12:57:24 -0500

    Improve test reliability
    Adds slightly more leeway in wait times and supports cross-platform select field interaction.
    
    Also fixes the GitHub action I just added so that it actually runs the things I wanted it to run.

--- tests/autosubmit.test.js
+++ tests/autosubmit.test.js
@@ -1,8 +1,17 @@
+const select = (page, selector, value) => page.evaluate((selector, value) => {
+  const select = document.querySelector(selector)
+  const option = select.querySelector(`option[value="${value}"]`)
+  option.selected = true
+  select.dispatchEvent(new Event('change', { bubbles: true }))
+}, selector, value)
+
 test('toggle', async () => {
   const page = await browser.newPage()
   await page.goto('http://localhost:4444/autosubmit.html')
-  await expect(page).toFill('.select-form-autosubmit', 'Bar')
+  await select(page, '.select-form-autosubmit', 'bar')
+  await page.waitForNavigation({ timeout: 200 })
   expect(page.url()).toContain('?foo=bar')
-  await expect(page).toFill('.select-field-autosubmit', 'Baz')
+  await select(page, '.select-field-autosubmit', 'baz')
+  await page.waitForNavigation({ timeout: 200 })
   expect(page.url()).toContain('?foo=baz')
 })
